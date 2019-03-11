22e4bde4afa05c59afb3ca3c8097d57183e07fe5
Saryn Caister <thegreenmajority@gmail.com>
2019-03-11 18:36:39 -0400

    improved navbar

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React from 'react';
 import HomeLink from "./homeLink/homeLink";
 import LoggedinLink from "./loggedinLink/loggedinLink";
 import ContactLink from "./contactLink/contactLink";
+import "./Navbar.css";
 
 function Navbar() {
     return (
