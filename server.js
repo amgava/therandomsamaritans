f60bd004743518755e7f1ab59ea15dd49ae58a5c
Pavani Vithala <pavani.vithala18@gmail.com>
2019-03-11 21:45:36 -0400

    Added DB

--- server.js
+++ server.js
@@ -1,19 +1,29 @@
 const express = require("express");
-const path = require("path");
-const PORT = process.env.PORT || 3001;
+var logger = require("morgan");
+const mongoose = require("mongoose");
+const routes = require("./routes");
 const app = express();
+const PORT = process.env.PORT || 3001;
 
+// Define middleware here
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 // Serve up static assets (usually on heroku)
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/build"));
 }
+// Add routes, both API and view
+app.use(routes);
+app.use(logger("dev"));
+// Connect to the Mongo DB
+mongoose.connect(process.env.MONGODB_URI || "mongodb://Pavani:Abhi2012@ds017205.mlab.com:17205/communityservice" , {useNewUrlParser: true });
 
-// Send every request to the React app
-// Define any API routes before this runs
-app.get("*", function(req, res) {
+app.get("*", (req, res) => {
   res.sendFile(path.join(__dirname, "./client/build/index.html"));
 });
 
+// Start the API server
 app.listen(PORT, function() {
-  console.log(`🌎 ==> API server now on port ${PORT}!`);
+  console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
 });
+
