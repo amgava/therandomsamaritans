31e344e991b80bd967b3ecb2b73bcde7ad898296
Pavani Vithala <pavani.vithala18@gmail.com>
2019-03-12 20:25:41 -0400

    Added DB Route for user

--- models/post.js
+++ models/post.js
@@ -1,13 +1,17 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-
 const postSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  link: { type: [String] },
-  postContent: { type: String, required: true },
-  date: { type: Date, default: Date.now }
-});
+  category: { type: String, required: true },
+  location: { type: String, required: true },
+  description: { type: [String],required: true },
+  contactNo: { type: String, required: true },
+  price: { type:double, default: '0'},
+  expiryDate: { type: Date, required: true },
+  User: {
+    type: Schema.Types.ObjectId,
+    ref: "User"
+  }
+  });
 
 const Post = mongoose.model("Post", postSchema);
 
