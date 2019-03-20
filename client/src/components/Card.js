d94e3195222f77752dd3018f09e83747bf181135
Amanda <38743110+amgava@users.noreply.github.com>
2019-03-19 20:39:04 -0400

    basic branch push after wipeout, adding card and modal

---
+++ client/src/components/Card.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Card() {
+  return (
+    <div className="card">
+      <div className="card-body">
+        <p className="card-text">
+
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Card;
