8ff66bc76eed9e8871a653091d46ce990c0af401
Pavani Vithala <pavani.vithala18@gmail.com>
2019-03-19 21:46:22 -0400

    Fixed Search Posts

--- routes/api/post.js
+++ routes/api/post.js
@@ -1,15 +1,15 @@
 const router = require("express").Router();
 const postController = require("../../controllers/postController");
 
-// Matches with "/api/books"
+// Matches with "/api/posts"
 router.route("/")
   .get(postController.findAll)
   .post(postController.create);
 
-// Matches with "/api/books/:id"
-router
-  .route("/:category")
-  .get(postController.findByCat)
+// Matches with "/api/posts/:id"
+//router
+ // .route("/:category")
+ // .get(postController.findByCat)
  // .put(postController.update)
  // .delete(postController.remove);
 
