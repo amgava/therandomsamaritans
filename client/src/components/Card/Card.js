93fe9c84e0e82428291451cb76078a4cc5d0f869
Amanda <38743110+amgava@users.noreply.github.com>
2019-03-21 19:37:04 -0400

    up

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -1,13 +1,36 @@
 import React from "react";
 import "./Card.css";
+import Location from "../Location";
 
-function Card() {
+function Card(props) {
   return (
     <div className="card">
-      <div className="card-body">
-        <p className="card-text">
-
-        </p>
+      <div className="card-body modal-card-body">
+        <div className="card-text">
+            <ul>
+            <li>
+                <strong>Category:</strong> {props.reqcategory}
+            </li>
+            <li>
+                <strong>Location:</strong> {props.reqlocation}
+            </li>
+            <li>
+                <strong>Description:</strong> {props.description}
+            </li>
+            <li>
+              <strong>Contact Number:</strong> {props.reqcontactNo}
+            </li>
+            <li>
+              <strong>Price:</strong> {props.reqprice}
+            </li>
+            <li className="hiddenClass">
+              <strong>Expiry:</strong> {props.reqexpiryDate}
+            </li>
+            <li className="hiddenClass">
+              <strong>User ID:</strong> {props.requserId}
+            </li>
+            </ul>
+        </div>
       </div>
     </div>
   );
