c19237ae59a2bdd9a3f3f32e90523bc442eefd8a
Saryn Caister <thegreenmajority@gmail.com>
2019-03-20 21:03:03 -0400

    lllll

--- routes/api/post.js
+++ routes/api/post.js
@@ -6,18 +6,10 @@ router.route("/")
   .get(postController.findAll)
   .post(postController.create);
 
-<<<<<<< HEAD
-// Matches with "/api/posts/:id"
-router
- // .route("/:category")
- // .get(postController.findByCat)
- .put(postController.update)
-=======
 // Matches with "/api/post/:id"
 router.route("/:id")
   .get(postController.findById)
  // .put(postController.update)
->>>>>>> 47b7f91239edee1731ebbd77029df2af2e18264f
  // .delete(postController.remove);
 
 module.exports = router;
