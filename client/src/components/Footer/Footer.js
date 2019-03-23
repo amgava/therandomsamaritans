fb783d1f613e5552d52250db45f6b7b26810baeb
Saryn Caister <thegreenmajority@gmail.com>
2019-03-22 21:45:15 -0400

    final touches

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,6 +5,10 @@ function Footer() {
     return (
         <div className="App-footer">
            <strong><img className="logocomet2" alt="homelink" src={require('./commetpink2.png') } /></strong>
+           <strong><img className="fabook" alt="homelink" src={require('./fabook.png') } /></strong>
+           <strong><img className="instagram" alt="homelink" src={require('./instagram.png') } /></strong>
+           <strong><img className="twit" alt="homelink" src={require('./twit.png') } /></strong>
+           <i class="fab fa-instagram"></i>
         </div>
     );
 }
